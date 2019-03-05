431ddf93f06dc09096e0efbad9b6b43f507376d2
Alexandre Defossez <alexandre.defossez@gmail.com>
2019-03-05 14:40:39 +0100

    added demo.py

---
+++ demo.py
@@ -0,0 +1,47 @@
+from treetable import treetable, table, group, leaf
+
+mytable = table([
+    group('info', [leaf('name'), leaf('index')]),
+    group(
+        'metrics',
+        align='>',
+        groups=[
+            leaf('speed', '.0f'),
+            leaf('accuracy', '.1%'),
+            leaf('special', '.1%', align='=')
+        ]),
+])
+
+lines = [
+    {
+        'info': {
+            'name': 'bob',
+            'index': 4
+        },
+        'metrics': {
+            'speed': 200,
+            'accuracy': 0.21,
+            'special': 0.1
+        }
+    },
+    {
+        'info': {
+            'name': 'alice',
+            'index': 2
+        },
+        'metrics': {
+            'speed': 67,
+            'accuracy': 0.45,
+            'special': 4.56
+        }
+    },
+]
+
+print(treetable(lines, mytable))
+mytable.shorten = True
+print("shortened version:")
+print(treetable(lines, mytable))
+
+# wrapping columns
+mytable.groups[0].groups[0].wrap = 3
+print(treetable(lines, mytable))
